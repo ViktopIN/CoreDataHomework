acf7a097bc08bc6df49fc262dcfd7111720229cc
Viktor <viktopiniaginya@gmail.com>
2022-08-26 13:39:06 +0300

    add tableView
    add tableView & customize, layout

--- CoreDataHomework/CoreDataHomework/Sources/Scene/ProfileAddingScene/View/ProfileAddingView.swift
+++ CoreDataHomework/CoreDataHomework/Sources/Scene/ProfileAddingScene/View/ProfileAddingView.swift
@@ -9,20 +9,39 @@ import UIKit
 import SnapKit
 
 class ProfileAddingView: UIViewController {
+// MARK: - Properties
+    private lazy var countOfCells = Int(10)
+    private var cellHeight: CGFloat = 45
 // MARK: - Views
     private lazy var addingProfileView: UIView = createAddingProfileView()
+    private var namesTableView = UITableView()
     
 // MARK: - Initialize
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableViewConfiguration()
+        
         setupHierarchy()
         setupView()
         setupLayout()
     }
     
 // MARK: - Private methods
+    private func tableViewConfiguration() {
+        namesTableView.translatesAutoresizingMaskIntoConstraints = false
+        namesTableView.layer.cornerRadius = 8
+        namesTableView.rowHeight = cellHeight
+        
+        namesTableView.register(CustomTableViewCell.self,
+                                forCellReuseIdentifier: CustomTableViewCell().identificator)
+        
+        namesTableView.dataSource = self
+//        namesTableView.delegate = self
+    }
     private func setupHierarchy() {
         view.addSubview(addingProfileView)
+        view.addSubview(namesTableView)
     }
     
     private func setupView() {
@@ -37,9 +56,43 @@ class ProfileAddingView: UIViewController {
     
     private func setupLayout() {
         addingProfileView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
             make.width.equalToSuperview()
             make.height.equalTo(110)
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+        
+        namesTableView.snp.makeConstraints { make in
+            make.top.equalTo(addingProfileView.snp.bottom).offset(32)
+            make.width.equalToSuperview().multipliedBy(0.91)
+            make.height.equalTo(getHeight())
+            make.centerX.equalToSuperview()
+        }
+    }
+}
+
+extension ProfileAddingView: UITableViewDataSource {
+// TODO: - finish correct table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        countOfCells = 3
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.namesTableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath)
+        
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = "nanana"
+        
+        return cell
+    }
+    
+    private func getHeight() -> CGFloat {
+        if countOfCells < 11 {
+            let height = CGFloat(countOfCells) * cellHeight
+            return height
+        } else {
+            return CGFloat(495)
         }
     }
 }
